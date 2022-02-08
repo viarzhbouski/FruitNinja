8b5f8c4633602bdf0b6e5c333ff0a3e8bd2ee35f
viarzhbouski <lordsith1996@gmail.com>
2022-02-08 16:53:06 +0300

    add config for bomb

---
+++ Assets/Scripts/Config/ExtraBlockConfig.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Extra Block", menuName = "ExtraBlock", order = 54)]
+public class ExtraBlockConfig : ScriptableObject
+{
+    [SerializeField]
+    private GameObject extraBlockPrefab;
+
+    [SerializeField]
+    private float fruitSpeed;
+    
+    [SerializeField]
+    private float fruitRotateSpeed;
+    
+    [SerializeField]
+    private float fragmentSpeed;
+
+    [SerializeField]
+    private float fragmentRotateSpeed;
+
+    public GameObject ExtraBlockPrefab
+    {
+        get { return extraBlockPrefab; }
+    }
+    
+    public float FruitSpeed
+    {
+        get { return fruitSpeed; }
+    }
+    
+    public float FruitRotateSpeed
+    {
+        get { return fruitRotateSpeed; }
+    }
+
+    public float FragmentSpeed
+    {
+        get { return fragmentSpeed; }
+    }
+    
+    public float FragmentRotateSpeed
+    {
+        get { return fragmentRotateSpeed; }
+    }
+}
